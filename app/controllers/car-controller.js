02c9e5ad2c1ba5e79e547d21a2eee357c215b5d0
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2023-04-17 16:07:46 +0700

    feat: add modal and notification delete

--- app/controllers/car-controller.js
+++ app/controllers/car-controller.js
@@ -1,89 +1,85 @@
 const { Car } = require("../models");
 
 exports.renderHome = async (req, res) => {
-    const cars = await Car.findAll();
-    res.render("index", { cars });
+	const cars = await Car.findAll();
+	res.render("index", { cars });
 }
 
 exports.renderAddCar = async (req, res) => {
-    res.render("pages/add-car");
+	res.render("pages/add-car");
 }
 
 exports.renderUpdateCar = async (req, res) => {
-    const car = req.car;
-    res.render("pages/edit-car", { car });
+	const car = req.car;
+	res.render("pages/edit-car", { car });
 }
 
 exports.renderByPk = async (req, res) => {
-    const id = 11;
-    const car = await Car.findByPk(id);
-    res.render("index", {car});   
+	const id = 11;
+	const car = await Car.findByPk(id);
+	res.render("index", {car});   
 }
 
 exports.getAllCars = async (req, res) => {
-    try {
-        const cars = await Car.findAll();
-        return cars;
-    } catch (error) {
-        res.status(500).json({
-            status: "Error",
-            message: error.message,
-        });
-    }
+	try {
+		const cars = await Car.findAll();
+		return cars;
+	} catch (error) {
+		res.status(500).json({
+			status: "Error",
+			message: error.message,
+		});
+	}
 };
 
 exports.getCar = async (req, res) => {
-    const car = req.car;
-    res.status(200).json({ data: car });
+	const car = req.car;
+	res.status(200).json({ data: car });
 };
 
 exports.addCar = async (req, res) => {
-    try {
-        const carData = {
-            name: req.body.name,
-            rent_per_day: req.body.rent_per_day,
-            size: req.body.size,
-            image: req.image
-        }
-        await Car.create({...carData});
-        req.flash("success", "Data Berhasil Disimpan");
-        res.redirect("/");
-    } catch (error) {
-        req.flash("error", error.message);
-        res.redirect("/");
-    }
+	try {
+		const carData = {
+			name: req.body.name,
+			rent_per_day: req.body.rent_per_day,
+			size: req.body.size,
+			image: req.image
+		}
+		await Car.create({...carData});
+		req.flash("success", "Data Berhasil Disimpan");
+		res.redirect("/");
+	} catch (error) {
+		req.flash("error", error.message);
+		res.redirect("/");
+	}
 };
 
 exports.updateCar = async (req, res) => {
-    try {
-        const carData = {
-            name: req.body.name,
-            rent_per_day: req.body.rent_per_day,
-            size: req.body.size,
-            image: req.image
-        }
-        await Car.update({...carData}, {
-            where: { id: req.params.id }
-        });
-        req.flash("success", "Data Berhasil Disimpan");
-        res.redirect("/");
-    } catch (error) {
-        res.status(500).json({
-            status: "Error",
-            message: error.message,
-        });
-    }
+	try {
+		const carData = {
+			name: req.body.name,
+			rent_per_day: req.body.rent_per_day,
+			size: req.body.size,
+			image: req.image
+		}
+		await Car.update({...carData}, {
+				where: { id: req.params.id }
+		});
+		req.flash("success", "Data Berhasil Disimpan");
+		res.redirect("/");
+	} catch (error) {
+		req.flash("error", error.message);
+		res.redirect("/");
+	}
 };
 
 exports.deleteCar = async (req, res) => {
-    try {
-        const id = req.params.id;
-        await Car.destroy({ where: { id } });
-        res.redirect("/");
-    } catch (error) {
-        res.status(500).json({
-            status: "Error",
-            message: error.message,
-        });
-    }
+	try {
+		await Car.destroy({ where: { id: req.params.id } });
+		req.flash("successDelete", "Data Berhasil Dihapus");
+		res.redirect("/");
+	} catch (error) {
+		req.flash("error", error.message);
+		res.redirect("/");
+	}
 };
--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,10 @@ const closeNotification = () => {
 	const notificationContainer = document.querySelector(".list-cars__notification")
 	notificationContainer.remove();
 }
+
+const setFormAction = () => {
+	const cardDeleteButton = document.querySelector(".list-card__card-delete");
+	const modalDeleteForm = document.getElementById("modal-delete-form");
+	const dataId = cardDeleteButton.getAttribute("data-attr");
+	modalDeleteForm.setAttribute("action", `/cars/delete/${dataId}`);
+}
